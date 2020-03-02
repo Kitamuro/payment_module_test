b0fe008a9634a8ebc79d77afebe56042ec74edcb
Art <th1s1sartakk@gmail.com>
2020-03-02 14:09:47 +0600

    init commit

---
+++ src/test/java/school/attractor/payment_module/PaymentModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package school.attractor.payment_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
