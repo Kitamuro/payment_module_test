71ea156e01445803f5860b961b12faef3c7fe330
Art <th1s1sartakk@gmail.com>
2020-03-05 16:53:44 +0600

    #10 Добавил поля в класс Commersant

--- src/main/java/school/attractor/payment_module/domain/commersant/Commersant.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/Commersant.java
@@ -1,4 +1,19 @@
 package school.attractor.payment_module.domain.commersant;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Commersant {
+    private String id;
+    private String name;
+
+
 }
+
+
