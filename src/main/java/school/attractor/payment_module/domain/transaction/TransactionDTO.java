df4590d18324da8223220c7701dc5df63d056f17
Art <th1s1sartakk@gmail.com>
2020-03-05 20:23:26 +0600

    #10 Добавил в TransactionDTO  поле со списком товаров (ItemDTO)

--- src/main/java/school/attractor/payment_module/domain/transaction/TransactionDTO.java
+++ src/main/java/school/attractor/payment_module/domain/transaction/TransactionDTO.java
@@ -2,6 +2,10 @@ package school.attractor.payment_module.domain.transaction;
 
 import lombok.*;
 import school.attractor.payment_module.domain.commersant.CommersantDTO;
+import school.attractor.payment_module.domain.item.ItemDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -10,21 +14,23 @@ import school.attractor.payment_module.domain.commersant.CommersantDTO;
 @Builder
 public class TransactionDTO {
     private String id;
-    private double orderAmount;
+    private double amount;
     private CommersantDTO commersantDTO;
     private String cardHolderName;
     private String cardNumber;
     private String cardExpiryDate;
     private String cardCvc;
+    private List<ItemDTO> items;
 
-    static TransactionDTO from(Transaction transaction) {
+    public static TransactionDTO from(Transaction transaction) {
         return builder()
                 .id(transaction.getId())
-                .orderAmount(transaction.getOrderAmount())
+                .amount(transaction.getAmount())
                 .cardHolderName(transaction.getCardHolderName())
                 .cardNumber(transaction.getCardNumber())
                 .cardExpiryDate(transaction.getCardExpiryDate())
                 .cardCvc(transaction.getCardCvc())
+                .items(transaction.getItems().stream().map(ItemDTO::from).collect(Collectors.toList()))
                 .build();
     }
 }
