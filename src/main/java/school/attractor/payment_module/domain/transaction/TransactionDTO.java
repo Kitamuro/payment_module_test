64f769fabe412f0d8a9fbfa0c85be94a80980f0c
Vyacheslav <leeteso97@gmail.com>
2020-03-29 18:26:51 +0600

    ADD in order-page JS functions for VALIDATION ERRORS from server

--- src/main/java/school/attractor/payment_module/domain/transaction/TransactionDTO.java
+++ src/main/java/school/attractor/payment_module/domain/transaction/TransactionDTO.java
@@ -4,10 +4,7 @@ import lombok.*;
 import school.attractor.payment_module.domain.commersant.CommersantDTO;
 import school.attractor.payment_module.domain.item.ItemDTO;
 
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 
@@ -27,27 +24,30 @@ public class TransactionDTO {
     private String email;
 
     @NotBlank(message = "Введите существующий номер телефона")
-    @Size(min = 10, max = 10, message = "Вы ввели неправильный номер")
+//    НА САМОМ ДЕЛЕ МЕНЬШЕ ТЕЛ НОМЕР
+    @Size(min = 15, max = 15, message = "Вы ввели неправильный номер")
     private String phone;
 
     @NotBlank(message = "Введите правильное имя на карте")
     private String cardHolderName;
 
     @NotBlank(message = "Введите номер существующей карты")
-//    @Digits(integer = 16, fraction = 0)
+//    @Digits(integer = 16, fraction = 0, message = "Вы ввели неправильную карту")
     @Size(min = 16, max = 16, message = "Вы ввели неправильную карту")
     private String CARD;
 
     @NotBlank(message = "Введите правильный месяц истечения срока вашей карты")
+    @Pattern(regexp="^(0|[1-9][0-9]*)$", message = "Вы ввели неправильные данные месяца")
     @Size(min = 1, max = 2)
     private String EXP;
 
     @NotBlank(message = "Введите правильный год истечения срока вашей карты")
+    @Pattern(regexp="^(0|[1-9][0-9]*)$", message = "Вы ввели неправильный данные года")
     @Size(min = 4, max = 4)
     private String EXP_YEAR;
 
     @NotBlank(message = "Введите правильный код на обратной стороне карты")
-    @Size(min = 3, max = 3)
+    @Pattern(regexp="^(0|[1-9][0-9]*)$", message = "Вы ввели неправильный код")
     private String CVC2;
 
     private List<ItemDTO> items;
--- src/main/java/school/attractor/payment_module/rest/ControllerRest.java
+++ src/main/java/school/attractor/payment_module/rest/ControllerRest.java
@@ -2,15 +2,23 @@ package school.attractor.payment_module.rest;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import school.attractor.payment_module.domain.ApacheHttp.ApacheHttpClientPost;
 import school.attractor.payment_module.domain.transaction.TransactionDTO;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -20,9 +28,9 @@ import static java.util.stream.Collectors.toList;
 public class ControllerRest {
 
     @PostMapping("/pay")
-    public HttpStatus mainController(@RequestBody @Valid TransactionDTO transactionDTO) throws IOException {
+    public HttpStatus mainController(@Valid @RequestBody  TransactionDTO transactionDTO) throws IOException {
         System.out.println(transactionDTO);
-        ApacheHttpClientPost.sendRequest (transactionDTO.getCARD (), transactionDTO.getEXP (), transactionDTO.getEXP_YEAR (), transactionDTO.getCVC2 ());
+//        ApacheHttpClientPost.sendRequest (transactionDTO.getCARD (), transactionDTO.getEXP (), transactionDTO.getEXP_YEAR (), transactionDTO.getCVC2 ());
         return HttpStatus.OK;
     }
 
