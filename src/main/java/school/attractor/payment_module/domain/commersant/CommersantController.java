264a77785819b241b18597e21392e27f614a12f7
Yussuf Ozdemir <mr.yuso01@gmail.com>
2020-04-06 15:28:38 +0600

    #25 Made commersant controller

--- src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
@@ -1,7 +1,9 @@
 package school.attractor.payment_module.domain.commersant;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@AllArgsConstructor
 public class CommersantController {
 }
