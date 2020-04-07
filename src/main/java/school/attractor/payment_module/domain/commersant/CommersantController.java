ec6e5cd9a2146de91c792d411001acd2602408cc
Art <th1s1sartakk@gmail.com>
2020-04-07 13:09:21 +0600

    #29 Добавил стартовую страницу кабинета коммерсанта ( main ) #25 Изменил mapping маршрута , подключил Thymeleaf

--- src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/commersant")
+@RequestMapping("/")
 public class CommersantController {
 
 }
