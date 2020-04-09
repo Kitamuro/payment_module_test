748d74941684701d04e15b3bd0810fdb2bb65f35
Art <th1s1sartakk@gmail.com>
2020-04-09 13:44:13 +0600

    #31 Вынес создание тестовых транзакций в отдельный класс

--- src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import school.attractor.payment_module.domain.transaction.Transaction;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -24,23 +21,10 @@ public class CommersantController {
 
     @GetMapping("/transactions")
     public String transaction(Model model) {
-
-        Transaction test = Transaction.builder()
-                .amount("100")
-                .CARD("333-333-333-333-333")
-                .cardHolderName("Test Name")
-                .CVC2("333")
-                .EXP("20/20")
-                .email("test@test")
-                .userName("test")
-                .build();
-
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add( test);
-        transactions.add( Transaction.builder().userName("test2").build());
-
-        model.addAttribute("transactions", transactions);
+        model.addAttribute("transactions", GenerateData.addTransaction());
         return "transactions";
     }
+
+
 
 }
