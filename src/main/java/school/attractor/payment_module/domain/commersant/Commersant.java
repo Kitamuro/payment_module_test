0640c9b2f5b96e4abf9e9283fadd0284bea0d70b
Art <th1s1sartakk@gmail.com>
2020-04-15 15:37:41 +0600

    #34 указал что сущность Commersant, Transaction это таблицы в БД

--- src/main/java/school/attractor/payment_module/domain/commersant/Commersant.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/Commersant.java
@@ -1,16 +1,21 @@
 package school.attractor.payment_module.domain.commersant;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import javax.persistence.*;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "commersants")
 public class Commersant {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 30)
     private String name;
 
 }
