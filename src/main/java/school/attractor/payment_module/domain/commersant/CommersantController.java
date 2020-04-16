15a6b8570fab7a8a6ebb0f5f4db99c7aaf565e1d
Zhanel <zhkhasenova@gmail.com>
2020-04-16 23:46:08 +0600

    #35 Добавленo CommersantController, TransactionService, GenerateData класс для проверки и внесены изменения в Transactions.html в части фронт-пагинации

--- src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/CommersantController.java
@@ -1,16 +1,29 @@
 package school.attractor.payment_module.domain.commersant;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import school.attractor.payment_module.domain.transaction.Transaction;
+import school.attractor.payment_module.domain.transaction.TransactionService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @Controller
 @AllArgsConstructor
 @RequestMapping
 public class CommersantController {
+
+    private final TransactionService transactionService;
 
 
     @GetMapping("/")
@@ -19,12 +32,29 @@ public class CommersantController {
         return "main";
     }
 
+//    @GetMapping("/transactions")
+//    public String transaction(Model model) {
+//        model.addAttribute("transactions", GenerateData.addTransaction());
+//        return "transactions";
+//    }
+
+
     @GetMapping("/transactions")
-    public String transaction(Model model) {
-        model.addAttribute("transactions", GenerateData.addTransaction());
+    public String getTransactions(Model model, @RequestParam("page")Optional<Integer> page, @RequestParam("size") Optional<Integer> size){
+        int currentPage = page.orElse ( 1 );
+        int pageSize = size.orElse ( 5 );
+        Page<Transaction> transactions = transactionService.getTransactions ( PageRequest.of(currentPage - 1, pageSize, Sort.by("shopName").descending() ) );
+        model.addAttribute ( "transactions", transactions );
+        int number = transactions.getNumber ( );
+        transactions.getSize ();
+        System.out.println ("number"+number );
+        model.addAttribute ( "number", number );
+        int totalPages = transactions.getTotalPages ();
+        if(totalPages > 0){
+            List<Integer> pageNumbers = IntStream.rangeClosed ( 1, totalPages ).boxed ().collect( Collectors.toList());
+            model.addAttribute ( "pageNumbers", pageNumbers );
+        }
         return "transactions";
+//    https://www.baeldung.com/spring-thymeleaf-pagination
     }
-
-
-
 }
--- src/main/java/school/attractor/payment_module/domain/transaction/TransactionService.java
+++ src/main/java/school/attractor/payment_module/domain/transaction/TransactionService.java
@@ -2,7 +2,14 @@ package school.attractor.payment_module.domain.transaction;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +20,23 @@ public class TransactionService {
     public void save(TransactionDTO transactionDTO) {
         Transaction transaction = Transaction.from(transactionDTO);
         transactionRepository.save(transaction);
+    }
+
+    public Page<Transaction> getTransactions(Pageable pageable){
+        List<Transaction> transactions = transactionRepository.findAll ( );
+        int pageSize = pageable.getPageSize ();
+        int currentPage = pageable.getPageNumber ();
+        int initialItem = currentPage * pageSize;
+        List<Transaction> transactionList;
+
+        if (transactions.size () < initialItem){
+            transactionList= Collections.emptyList ();
+        }else{
+            int lastIndexofItem = Math.min(initialItem + pageSize, transactions.size ());
+            transactionList = transactions.subList ( initialItem, lastIndexofItem );
+        }
+        return new PageImpl<Transaction> (transactionList, PageRequest.of(currentPage, pageSize ), transactions.size ());
+//    https://www.baeldung.com/spring-thymeleaf-pagination
     }
 
 }
