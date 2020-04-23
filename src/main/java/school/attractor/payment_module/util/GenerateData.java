74741f3dee0ad43571c8baf42427fb3b54738ff7
Art <th1s1sartakk@gmail.com>
2020-04-23 19:51:18 +0600

    #39  Добавил рандом  для цены, номера заказа, названия магазина, имя покупателя в класс где генерируются транзакции

--- src/main/java/school/attractor/payment_module/util/GenerateData.java
+++ src/main/java/school/attractor/payment_module/util/GenerateData.java
@@ -2,34 +2,36 @@ package school.attractor.payment_module.util;
 
 import school.attractor.payment_module.domain.transaction.Transaction;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 
 class GenerateData {
 
     static List<Transaction> addTransaction() {
-        List<Transaction> transactions = new ArrayList<>();
 
+        Random random = new Random();
+        List<Transaction> transactions = new ArrayList<>();
+        List<String> shops = Arrays.asList("Sulpak", "Все для дома", "Хозяюшка","Технодом","Магнум","Aliexpress","Lamoda");
+        List<String> userName = Arrays.asList("Artur","Бакытжан","Кирил","Вячеслав","Вор","Test");
         for (int i = 0; i < 30; i++) {
+            int randomAmount = random.nextInt(80000);
+            int randomId = random.nextInt(5000);
+
             Transaction test = Transaction.builder()
-                    .amount("100")
+                    .amount(String.valueOf(randomAmount))
                     .CARD("2214-3112-333-0987-5672")
-                    .cardHolderName("Test Name")
+                    .cardHolderName(userName.get(random.nextInt(userName.size())))
                     .CVC2("7800")
                     .EXP("20/20")
                     .email("test@test")
-                    .userName("test")
+                    .userName(userName.get(random.nextInt(userName.size())))
                     .date(new Date())
-                    .shopName("Все для дома")
+                    .shopName(shops.get(random.nextInt(shops.size())))
                     .status("Подтвержден")
-                    .orderId("1277")
+                    .orderId(String.valueOf(randomId))
                     .build();
             transactions.add(test);
         }
-
         return transactions;
-
     }
 }
