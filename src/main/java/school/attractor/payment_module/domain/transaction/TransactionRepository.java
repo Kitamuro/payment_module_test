3cf6299619fee91e47d8fd137a7caeb4a29ddad2
Vyacheslav <leeteso97@gmail.com>
2020-04-29 14:11:03 +0600

    #48 Поменял код стилей, внес мелкие изменения и добавил выборку в статусе оплаты

--- src/main/java/school/attractor/payment_module/domain/transaction/TransactionRepository.java
+++ src/main/java/school/attractor/payment_module/domain/transaction/TransactionRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     Page<Transaction> findAllByOrderId(String id, Pageable pageable);
-    Page<Transaction> findAllByOrderIdAndStatus(String id, String status, Pageable pageable);
-    Page<Transaction> findAllByOrderIdAndShopName(String id, String shopName, Pageable pageable);
-    Page<Transaction> findAllByStatus(String status, Pageable pageable);
-    Page<Transaction> findAllByShopName(String shopName, Pageable pageable);
-    Page<Transaction> findAllByStatusAndShopName (String status, String shopName, Pageable pageable);
-    Page<Transaction> findAllByOrderIdAndStatusAndShopName (String id, String status, String shopName, Pageable pageable);
+    Page<Transaction> findAllByOrderIdAndStatusContaining(String id, String status, Pageable pageable);
+    Page<Transaction> findAllByOrderIdAndShopNameContaining(String id, String shopName, Pageable pageable);
+    Page<Transaction> findAllByStatusContaining(String status, Pageable pageable);
+    Page<Transaction> findAllByShopNameContaining(String shopName, Pageable pageable);
+    Page<Transaction> findAllByStatusAndShopNameContaining (String status, String shopName, Pageable pageable);
+    Page<Transaction> findAllByOrderIdAndStatusAndShopNameContaining (String id, String status, String shopName, Pageable pageable);
 }
