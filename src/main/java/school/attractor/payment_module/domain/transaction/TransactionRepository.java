b4d10612e22b0d200b8f4112b63fd4e62e4117bd
Art <th1s1sartakk@gmail.com>
2020-05-04 00:58:33 +0600

    #50 Добавил SQL-запрос который собирает из базы все транзакции по номеру заказа

--- src/main/java/school/attractor/payment_module/domain/transaction/TransactionRepository.java
+++ src/main/java/school/attractor/payment_module/domain/transaction/TransactionRepository.java
@@ -3,17 +3,24 @@ package school.attractor.payment_module.domain.transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
     Page<Transaction> findAllByOrderId(String id, Pageable pageable);
+    List<Transaction> findAllByOrderId(String id);
     Page<Transaction> findAllByOrderIdAndStatusContaining(String id, String status, Pageable pageable);
     Page<Transaction> findAllByOrderIdAndShopNameContaining(String id, String shopName, Pageable pageable);
     Page<Transaction> findAllByStatusContaining(String status, Pageable pageable);
     Page<Transaction> findAllByShopNameContaining(String shopName, Pageable pageable);
     Page<Transaction> findAllByStatusAndShopNameContaining (String status, String shopName, Pageable pageable);
     Page<Transaction> findAllByOrderIdAndStatusAndShopNameContaining (String id, String status, String shopName, Pageable pageable);
+
+    @Query(value = "SELECT  SUM(t.amount) as total  FROM  transactions  as t WHERE t.order_id=:orderId", nativeQuery = true)
+    Integer getSum(@Param("orderId") String orderId);
 }
