b5b6e849d2c3905da363d5e6d3f0318c83cf95ac
Art <th1s1sartakk@gmail.com>
2020-05-23 19:48:04 +0600

    #63 Добавил нужны поля в DTO  с проверкой валидации

--- src/main/java/school/attractor/payment_module/domain/commersant/Commersant.java
+++ src/main/java/school/attractor/payment_module/domain/commersant/Commersant.java
@@ -5,6 +5,8 @@ import lombok.*;
 import school.attractor.payment_module.domain.shop.Shop;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +28,41 @@ public class Commersant {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "commersant")
     private List<Shop> shops = new ArrayList<> ();
 
+    @NotBlank
+    @Size(max=30)
+    private String bik;
 
+    @NotBlank
+    @Size(max=30)
+    private String bin;
 
+    @NotBlank
+    @Size(max=30)
+    private String directorIdentityCard;
+
+    @NotBlank
+    @Size(max=30)
+    private String directorName;
+
+    @NotBlank
+    @Size(max=30)
+    private String kbe;
+
+    @NotBlank
+    @Size(max=30)
+    private String organizationName;
+
+    public static Commersant from(CommersantDTO commersant) {
+        return  builder()
+             .name(commersant.getName())
+                .bik(commersant.getBik())
+                .bin(commersant.getBin())
+                .directorIdentityCard(commersant.getDirectorIdentityCard())
+                .directorName(commersant.getDirectorName())
+                .kbe(commersant.getKbe())
+                .organizationName(commersant.getOrganizationName())
+                .build();
+    }
 
 }
 
