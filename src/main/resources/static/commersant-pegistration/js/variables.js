f3c35efe2bab803160147225741dd9ac9a74cc74
Art <th1s1sartakk@gmail.com>
2020-05-24 15:36:57 +0600

    #63 Отрефакторил код, вынес статичные значения в переменные и в отдельный JS

---
+++ src/main/resources/static/commersant-pegistration/js/variables.js
@@ -0,0 +1,5 @@
+const FORM = window['form'];
+const BASE_URL = 'http://localhost:8080';
+const REG_URL = BASE_URL + '/registration';
+const VALIDATION_SUCCESS = 'Validation Successful';
+const MESSAGE_SUCCESS = 'Регистрация прошла успешно';
