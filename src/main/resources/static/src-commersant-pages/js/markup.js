37e1ed18e752d68ff349ec8be56356b1e093f449
Art <th1s1sartakk@gmail.com>
2020-05-28 21:02:09 +0600

    #65  Изменил дизайн блока с подробной информацией о заказе

--- src/main/resources/static/src-commersant-pages/js/markup.js
+++ src/main/resources/static/src-commersant-pages/js/markup.js
@@ -4,8 +4,8 @@ function addFormRefund(orderDTO) {
                     <form action="/sendRequest" method="post">
                     <input type="hidden" value="${orderDTO.id}" name="orderId">
                     <input type="hidden" value="REFUND" name="type">
-                    <input type="number"  placeholder="введите сумму" name="amount" oninput="validateSum(value)" >
-                    <button id="returnButton" type="submit">вернуть</button>
+                    <input type="number" class="refund-input" placeholder="введите сумму" name="amount" oninput="validateSum(value)" >
+                    <button id="returnButton" class="returnButton" type="submit">вернуть</button>
                     </form>
                     </div>`;
     const div = document.createElement('div');
@@ -19,8 +19,8 @@ function addFormAuth(orderDTO) {
                     <form action="/sendRequest" method="post">
                     <input type="hidden" value="${orderDTO.id}" name="orderId">
                     <input type="hidden" value="AUTH" name="type">
-                    <input type="number"  placeholder="введите сумму" name="amount" oninput="validateSum(value)" >
-                    <button id="returnButton" type="submit">подтвердить</button>
+                    <input type="number" class="refund-input" placeholder="введите сумму" name="amount" oninput="validateSum(value)" >
+                    <button id="returnButton" class="returnButton" type="submit">подтвердить</button>
                     </form>
                     </div>`;
     const div = document.createElement('div');
@@ -30,11 +30,12 @@ function addFormAuth(orderDTO) {
 
 function print(orderDTO) {
     infoBlock.innerHTML = `<div id="close-icon" class="close"> x </div>
-                           <div class="id"> ${orderDTO.orderId} </div>
-                           <div> Остаток : ${orderDTO.residual} </div>
+                           <div class="order-label"> Заказ </div>
+                           <div class="orderId"> № ${orderDTO.orderId} </div>
                            <div class="status">${orderDTO.status} </div>
+                           <div class="residual"> Остаток : ${orderDTO.residual} </div>
                            <div class="amount">${orderDTO.amount}  TГ </div>
-                           <div class="title"> Платежная информация </div>
+                           <div class="order-title"> Платежная информация </div>
                            <div class="row"> <div>Дата оплаты</div>  <div> ${dateFormat(orderDTO.date)} </div> </div>
                            <div class="row"> <div>Магазин</div>  <div> ${orderDTO.shopName} </div> </div>
                            <div class="order-title">Спосаб оплаты</div>
@@ -43,7 +44,7 @@ function print(orderDTO) {
                            <div class="order-title">Информация о покупке</div>
                            <div class="row"> <div>Телефон</div>  <div>${orderDTO.phone} </div> </div>
                            <div class="row"> <div>Почта</div>  <div> ${orderDTO.email} </div> </div>
-                           <div>  История платежей </div>
+                           <div class="order-title">  История платежей </div>
                            <div>
                                ${orderDTO.transactions.map(transaction => `<div> ${dateFormat(transaction.date)} <span> ${transaction.amount} тг </span> ${transaction.type}  </div>`).join('')}
                            </div>`.trim();
@@ -52,7 +53,7 @@ function print(orderDTO) {
         const formDiv = addFormRefund(orderDTO);
         amount.after(formDiv)
     }
-    if(orderDTO.status === 'RESERVED' ) {
+    if (orderDTO.status === 'RESERVED') {
         const formDivAuth = addFormAuth(orderDTO);
         amount.after(formDivAuth);
     }
